2beefab574b7ef7cb9ab87823c809e1c1e25a723
Philip Luppens <philip.luppens@gmail.com>
2014-03-16 19:47:37 +0100

    Added standalone assembly for Tomcat.

---
+++ src/main/resources/setenv.bat
@@ -0,0 +1,17 @@
+@REM
+@REM Copyright 2013 European Parliament
+@REM
+@REM Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+@REM You may not use this work except in compliance with the Licence.
+@REM You may obtain a copy of the Licence at:
+@REM
+@REM http://joinup.ec.europa.eu/software/page/eupl
+@REM
+@REM Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the Licence for the specific language governing permissions and limitations under the Licence.
+@REM
+
+JAVA_OPTS="-server -Xmx512m -Xms128m -XX:PermSize=128m"
+
+CATALINA_OPTS="${JAVA_OPTS}"
